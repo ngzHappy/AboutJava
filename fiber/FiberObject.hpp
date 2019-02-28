8a8bcf1e4e78b927b03f56d65d4a85f9dbc0d4eb
ngzHappy <zhaixueqiang@hotmail.com>
2019-02-28 18:40:36 +0800

    +test code

--- fiber/FiberObject.hpp
+++ fiber/FiberObject.hpp
@@ -6,6 +6,7 @@
 #include <array>
 #include <atomic>
 #include <memory>
+#include <cassert>
 #include <optional>
 #include <exception>
 #include <shared_mutex>
@@ -22,24 +23,41 @@ namespace fiber_function_basic {
         Calling,
     };
 
+    template<typename TParent, typename FiberDataType = FiberData >
+    class FiberObject;
     class FiberData {
     public:
         using fiber_type = boost::context::fiber;
+    private:
         fiber_type * runtimeFiber{ nullptr };
         FiberState state{ FiberState::Yield };
         std::optional<fiber_type> fiber;
+        template<typename, typename>
+        friend class ::fiber_function_basic::FiberObject;
+    public:
+        inline FiberData() = default;
+        inline virtual ~FiberData() = default;
+    public:
+        FiberData(const FiberData &) = delete;
+        FiberData(FiberData &&) = delete;
+        FiberData&operator=(const FiberData &) = delete;
+        FiberData&operator=(FiberData &&) = delete;
     };
 
     /*
+    TParent£º
         void doCall() ;
         void doOnException(std::exception_ptr) noexcept;
+    FiberDataType£º
+        child of FiberData
     */
-    template<typename TParent>
+    template<typename TParent, typename FiberDataType >
     class FiberObject {
+        static_assert(std::is_base_of_v<FiberData, FiberDataType>);
     public:
         using fiber_type = FiberData::fiber_type;
     private:
-        std::shared_ptr< FiberData > mmmData;
+        std::shared_ptr< FiberDataType > mmmData;
     public:
         inline FiberObject(std::size_t = 1024uLL * 1024uLL * 10uLL);
     public:
@@ -49,6 +67,8 @@ namespace fiber_function_basic {
         FiberObject&operator=(FiberObject &&) = default;
     public:
         inline FiberState getFiberState() const;
+    public:
+        inline std::shared_ptr< FiberDataType > getFiberData() const;
     public:
         inline void _fiberCall() const;
     private:
@@ -60,70 +80,76 @@ namespace fiber_function_basic {
         inline FiberState callThis();
     };
 
-    template<typename TParent>
-    inline FiberObject<TParent>::FiberObject(std::size_t argSizeOfFiber) {
-        mmmData = std::make_shared<FiberData>();
+    template<typename TParent, typename FiberDataType >
+    inline FiberObject<TParent, FiberDataType>::FiberObject(std::size_t argSizeOfFiber) {
+        mmmData = std::make_shared<FiberDataType>();
         using this_stack =
             boost::context::protected_fixedsize_stack;
-        mmmData->fiber.emplace(
+        mmmData->FiberData::fiber.emplace(
             std::allocator_arg,
             this_stack{ argSizeOfFiber },
             [this](fiber_type && argFiber)->fiber_type {
-            mmmData->runtimeFiber = &argFiber;
+            mmmData->FiberData::runtimeFiber = &argFiber;
             return FiberObject::thisCall(std::move(argFiber));
         });
     }
 
-    template<typename TParent>
-    inline typename FiberObject<TParent>::fiber_type FiberObject<TParent>::thisCall(
-        fiber_type && argFiber) {
+    template<typename TParent, typename FiberDataType>
+    inline typename FiberObject<TParent, FiberDataType>::fiber_type
+        FiberObject<TParent, FiberDataType>::thisCall(
+            fiber_type && argFiber) {
         try {
             static_cast<TParent *>(this)
                 ->doCall();
         } catch (...) {
-            mmmData->state = FiberState::Error;
+            mmmData->FiberData::state = FiberState::Error;
             static_cast<TParent *>(this)
                 ->doOnException(std::current_exception());
         }
         return std::move(argFiber);
     }
 
-    template<typename TParent>
-    inline FiberState FiberObject<TParent>::callThis() {
-        auto & varState = mmmData->state;
+    template<typename TParent, typename FiberDataType>
+    inline FiberState FiberObject<TParent, FiberDataType>::callThis() {
+        auto & varState = mmmData->FiberData::state;
         if (varState != FiberState::Yield) {
             return varState;
         }
         varState = FiberState::Calling;
-        *(mmmData->fiber) = std::move(*(mmmData->fiber)).resume();
+        *(mmmData->FiberData::fiber) = std::move(*(mmmData->FiberData::fiber)).resume();
         if (varState != FiberState::Calling) {
             return varState;
         }
         return (varState = FiberState::Finished);
     }
 
-    template<typename TParent>
-    inline void FiberObject<TParent>::operator()() const {
+    template<typename TParent, typename FiberDataType>
+    inline void FiberObject<TParent, FiberDataType>::operator()() const {
         auto varThis =
             const_cast<FiberObject *>(this);
         varThis->FiberObject::callThis();
     }
 
-    template<typename TParent>
-    inline void FiberObject<TParent>::_fiberCall() const {
+    template<typename TParent, typename FiberDataType>
+    inline void FiberObject<TParent, FiberDataType>::_fiberCall() const {
         (*this).FiberObject::operator()();
     }
 
-    template<typename TParent>
-    inline void FiberObject<TParent>::_fiberYield() const {
+    template<typename TParent, typename FiberDataType>
+    inline void FiberObject<TParent, FiberDataType>::_fiberYield() const {
         assert(mmmData->runtimeFiber);
-        mmmData->state = FiberState::Yield;
-        *(mmmData->runtimeFiber) = std::move(*(mmmData->runtimeFiber)).resume();
+        mmmData->FiberData::state = FiberState::Yield;
+        *(mmmData->FiberData::runtimeFiber) = std::move(*(mmmData->FiberData::runtimeFiber)).resume();
     }
 
-    template<typename TParent>
-    inline FiberState FiberObject<TParent>::getFiberState() const {
-        return mmmData->state;
+    template<typename TParent, typename FiberDataType>
+    inline FiberState FiberObject<TParent, FiberDataType>::getFiberState() const {
+        return mmmData->FiberData::state;
+    }
+
+    template<typename TParent, typename FiberDataType>
+    inline std::shared_ptr< FiberDataType > FiberObject<TParent, FiberDataType>::getFiberData() const {
+        return mmmData;
     }
 
 }/*namespace fiber_function_basic*/
