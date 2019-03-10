7a1fbc953788763659bf06363cda4709a9b20445
ngzHappy <zhaixueqiang@hotmail.com>
2019-03-10 14:16:03 +0800

    +gc

---
+++ gc/GCNode.cpp
@@ -0,0 +1,211 @@
+﻿#include <set>
+#include <list>
+#include <cassert>
+#include "GCNode.hpp"
+
+namespace sstd {
+
+    template<typename T, typename F>
+    inline void destoryAll(T & arg, F && fun) {
+        auto varPos = arg.rbegin();
+        auto varEnd = arg.rend();
+        for (; varPos != varEnd; ++varPos) {
+            fun(*varPos);
+        }
+    }
+
+    GCNode::GCNode() :
+        mmmGCState{ GCNodeState::White },
+        mmmManager{ nullptr }{
+    }
+
+    GCNode::~GCNode() {
+    }
+
+    std::size_t GCNode::doTheClassSize() const noexcept {
+        return sizeof(*this);
+    }
+
+    GCNodeChildIterator * GCNode::doTheClassChildren() const noexcept {
+        static GCNodeChildIterator varAns;
+        return &varAns;
+    }
+
+    GCNodeChildIterator::GCNodeChildIterator() {
+    }
+
+    GCNodeChildIterator::~GCNodeChildIterator() {
+    }
+
+    GCNode * GCNodeChildIterator::next() noexcept {
+        return nullptr;
+    }
+
+    class GCNodeManagerPrivate {
+    public:
+        std::set<GCNode *> roots;
+        std::list<GCNode *> grayNodes;
+        std::list<GCNode *> allNodes;
+        std::size_t currentMemorySize{ 0 };
+        std::size_t oldMemorySize{ 0 };
+        inline GCNodeManagerPrivate();
+        inline ~GCNodeManagerPrivate();
+    };
+
+    inline GCNodeManagerPrivate::GCNodeManagerPrivate() {
+    }
+
+    inline GCNodeManagerPrivate::~GCNodeManagerPrivate() {
+        destoryAll(allNodes, [](GCNode * v) { delete v; });
+    }
+
+    GCNodeManager::GCNodeManager() {
+        using TheDataType = decltype(mmmTheData);
+        static_assert((sizeof(GCNodeManagerPrivate) + alignof(GCNodeManagerPrivate))
+            <= sizeof(TheDataType));
+        {
+            void * varPointer = mmmTheData.data();
+            auto varSize = mmmTheData.size();
+            varPointer = std::align(
+                alignof(GCNodeManagerPrivate),
+                sizeof(GCNodeManagerPrivate),
+                varPointer,
+                varSize);
+            thePrivate = ::new(varPointer) GCNodeManagerPrivate;
+        }
+    }
+
+    GCNodeManager::~GCNodeManager() {
+        std::destroy_at(thePrivate);
+    }
+
+    void GCNodeManager::markAsGray(GCNode * arg) noexcept {
+        thePrivate->grayNodes.push_back(arg);
+        arg->mmmGCState = GCNodeState::Gray;
+    }
+
+    void GCNode::theClassAddRef(GCNode *arg) noexcept {
+        assert(theClassManager());
+
+        if (arg->mmmManager == nullptr) {
+            theClassManager()->addANode(arg);
+        }
+
+        if (mmmGCState == GCNodeState::Black) {
+            if (arg->mmmGCState == GCNodeState::White) {
+                theClassManager()->markAsGray(arg);
+            }
+        }
+        doTheClassAddRef(arg);
+    }
+
+    void GCNode::doTheClassAddRef(GCNode *) noexcept {
+        /*do it yourself...*/
+    }
+
+    void GCNodeManager::nextStep() noexcept {
+
+        const auto & varRoots = thePrivate->roots;
+        auto & varGray = thePrivate->grayNodes;
+        auto & varAll = thePrivate->allNodes;
+
+        for (const auto & varI : varRoots) {
+            if (varI->mmmGCState == GCNodeState::White) {
+                markAsGray(varI);
+            }
+        }
+
+        while (!varGray.empty()) {
+
+            auto varValue = varGray.front();
+            varGray.pop_front();
+
+            varValue->mmmGCState = GCNodeState::Black;
+
+            {
+                auto varI = varValue->theClassChildren();
+                while (auto varNext = varI->next()) {
+                    if (varNext->mmmGCState == GCNodeState::White) {
+                        markAsGray(varNext);
+                    }
+                }
+            }
+
+        }
+
+        std::list< GCNode * > varDoNotDelete;
+        std::list< GCNode * > varDelete;
+
+        while (!varAll.empty()) {
+
+            auto varPos = varAll.begin();
+            if ((*varPos)->mmmGCState != GCNodeState::White) {
+                varDoNotDelete.splice(varDoNotDelete.end(),
+                    varAll,
+                    varPos);
+                (*varPos)->mmmGCState = GCNodeState::White;
+            } else {
+                varDelete.splice(varDelete.end(),
+                    varAll,
+                    varPos);
+            }
+
+        }
+
+        varAll = std::move(varDoNotDelete);
+
+        {
+            auto varRemove = [this](GCNode * v) {
+                removeANode(v);
+            };
+            destoryAll(varDelete, varRemove);
+        }
+
+        thePrivate->oldMemorySize = thePrivate->currentMemorySize;
+
+    }
+
+    void GCNodeManager::addANode(GCNode * arg) noexcept {
+        arg->setGCNodeManager(this);
+        thePrivate->allNodes.push_back(arg);
+        thePrivate->currentMemorySize += arg->theClassSize();
+        if (thePrivate->currentMemorySize > thePrivate->oldMemorySize) {
+            if (newMeomorySize() > (1024uLL * 1024uLL * 64uLL)) {
+                this->nextStep();
+            }
+        }
+    }
+
+    void GCNodeManager::removeANode(GCNode * arg) noexcept {
+        thePrivate->currentMemorySize -= arg->theClassSize();
+        delete arg;
+    }
+
+    void GCNodeManager::markAsRoot(GCNode * arg) noexcept {
+        if (arg->mmmManager == nullptr) {
+            addANode(arg);
+        }
+        thePrivate->roots.insert(arg);
+        if (arg->mmmGCState == GCNodeState::White) {
+            markAsGray(arg);
+        }
+    }
+
+    void GCNodeManager::removeRoot(GCNode * arg) noexcept {
+        thePrivate->roots.erase(arg);
+    }
+
+    std::size_t GCNodeManager::newMeomorySize() const noexcept {
+        const auto varOld = thePrivate->oldMemorySize;
+        const auto varNew = thePrivate->currentMemorySize;
+        if (varNew > varOld) {
+            return varNew - varOld;
+        }
+        return varOld - varNew;
+    }
+
+}/*namespace sstd*/
+
+
+
+
---
+++ gc/GCNode.hpp
@@ -0,0 +1,125 @@
+﻿#pragma once
+
+#include <array>
+#include <memory>
+#include <utility>
+#include <type_traits>
+
+namespace sstd {
+
+    class GCNode;
+    class GCNodeManager;
+    class GCNodeChildIterator;
+    class GCNodeManagerPrivate;
+
+    enum class GCNodeState : std::size_t {
+        Black,
+        White,
+        Gray,
+    };
+
+    class GCNode {
+        GCNodeState mmmGCState;
+        GCNodeManager * mmmManager;
+        friend class ::sstd::GCNodeManager;
+    protected:
+        GCNode();
+    public:
+        virtual ~GCNode();
+    public:
+        inline std::size_t theClassSize() const noexcept;
+        inline GCNodeChildIterator * theClassChildren() const noexcept;
+        inline GCNodeManager * theClassManager() const noexcept;
+        void theClassAddRef(GCNode *) noexcept;
+    protected:
+        virtual std::size_t doTheClassSize() const noexcept;
+        virtual GCNodeChildIterator * doTheClassChildren() const noexcept;
+        virtual void doTheClassAddRef(GCNode *) noexcept;
+    public:
+        GCNode(const GCNode &) = delete;
+        GCNode(GCNode&&) = delete;
+        GCNode&operator=(const GCNode &) = delete;
+        GCNode&operator=(GCNode&&) = delete;
+    private:
+        inline void setGCNodeManager(GCNodeManager *) noexcept;
+    };
+
+    class GCNodeChildIterator {
+    public:
+        GCNodeChildIterator();
+        virtual ~GCNodeChildIterator();
+        virtual GCNode * next() noexcept;
+    };
+
+    class GCNodeManager {
+        std::array< std::uint8_t, 256 > mmmTheData;
+        GCNodeManagerPrivate * thePrivate;
+        friend class ::sstd::GCNode ;
+    public:
+        GCNodeManager();
+        virtual ~GCNodeManager();
+    public:
+        template<typename T, typename ... Args>
+        inline T * createNode(Args &&...);
+    public:
+        void nextStep() noexcept;
+        void markAsRoot(GCNode *) noexcept;
+        void removeRoot(GCNode *) noexcept;
+        std::size_t newMeomorySize() const noexcept;
+    private:
+        void addANode(GCNode *) noexcept;
+        void removeANode(GCNode *) noexcept;
+        void markAsGray(GCNode *) noexcept;
+    public:
+        GCNodeManager(const GCNodeManager &) = delete;
+        GCNodeManager(GCNodeManager &&) = delete;
+        GCNodeManager&operator=(const GCNodeManager &) = delete;
+        GCNodeManager&operator=(GCNodeManager &&) = delete;
+    };
+
+
+}/*NAMESPACE SSTD*/
+
+namespace sstd {
+
+    inline std::size_t GCNode::theClassSize() const noexcept {
+        return this->doTheClassSize();
+    }
+
+    inline GCNodeChildIterator * GCNode::theClassChildren() const noexcept {
+        return doTheClassChildren();
+    }
+
+    inline void GCNode::setGCNodeManager(GCNodeManager * arg) noexcept {
+        this->mmmManager = arg;
+    }
+
+    inline GCNodeManager * GCNode::theClassManager() const noexcept {
+        return this->mmmManager;
+    }
+
+    template<typename T, typename ... Args>
+    inline T * GCNodeManager::createNode(Args &&... args) {
+
+        static_assert( false==std::is_reference_v<T> );
+
+        T * varAns;
+
+        if constexpr (std::is_constructible_v<T, Args &&...> && (sizeof...(Args) > 0)) {
+            varAns = new T(std::forward<Args>(args)...);
+        } else {
+            varAns = new T{ std::forward<Args>(args)... };
+        }
+
+        {
+            GCNode * varNode = varAns;
+            this->addANode(varNode);
+        }
+
+        return varAns;
+
+    }
+
+}/*NAMESPACE SSTD*/
+
+
